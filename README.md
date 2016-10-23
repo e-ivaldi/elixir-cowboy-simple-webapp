08477c5c2d540e2a8594fe332b2e5f6313354211
Emanuele Ivaldi <emanuele.ivaldi@gmail.com>
2016-10-23 14:12:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# HelloWorld
+
+Resolve dependencies:
+mix deps.get
+
+Start appliction:
+run --no-halt
+
+Registered endpoint:
+http://localhost:8080
---
+++ lib/hello_world/handler.ex
@@ -0,0 +1,17 @@
+defmodule HelloWorld.Handler do
+  @moduledoc false
+  def init({:tcp, :http}, req, opts) do
+    headers = [{"content-type", "text/plain"}]
+    body = "Hello world!"
+    {:ok, resp} = :cowboy_req.reply(200, headers, body, req)
+    {:ok, resp, opts}
+  end
+
+  def handle(req, state) do
+    {:ok, req, state}
+  end
+
+  def terminate(_reason, _req, _state) do
+    :ok
+  end
+end
